c75ef6a808c5ad60f6b60507f59d009d451665ee
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2022-11-16 10:21:00 +0100

    WIP use sentry images for test services

--- push_images.sh
+++ push_images.sh
@@ -1,8 +1,10 @@
+set -ex
 
 BASE_NAME="europe-west3-docker.pkg.dev/sentry-st-testing/public/open-telemetry-demo"
 VERSION="v1.0.0"
 
-IMAGES=( cartService checkoutService currencyService emailService featureflagService frontend frontendProxy loadgenerator paymentService productCatalogService recommendationService shippingService quoteService adService)
+#IMAGES=( cartService checkoutService currencyService emailService featureflagService frontend frontendProxy loadgenerator paymentService productCatalogService recommendationService shippingService quoteService adService)
+IMAGES=( cartService )
 
 for DIR_NAME in "${IMAGES[@]}"
 do
@@ -10,11 +12,7 @@ do
   IMAGE=$(echo "$DIR_NAME" |  tr '[:upper:]' '[:lower:]' )
   IMAGE_NAME=${BASE_NAME}:${VERSION}-${IMAGE}
   echo "Building ${IMAGE} ..."
-  docker buildx build  --platform linux/amd64 -f ./src/${DIR_NAME}/Dockerfile -t  ${IMAGE_NAME} .
+  docker buildx build  --platform linux/amd64 -f ./src/${DIR_NAME}/src/Dockerfile -t  ${IMAGE_NAME} .
   echo "Pushing ${IMAGE} to ${IMAGE_NAME} "
   docker push ${IMAGE_NAME}
 done
-
-
-
-docker buildx build  --platform linux/amd64 -f ./src/adService/Dockerfile -t  europe-west3-docker.pkg.dev/sentry-st-testing/public/open-telemetry-demo:v1.0.0-adservice
