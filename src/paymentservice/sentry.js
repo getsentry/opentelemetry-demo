f4821b6d71fd8807a7d92d4a2a381ee2538dd1bd
Abhijeet Prasad <aprasad@sentry.io>
2022-11-24 15:27:10 +0100

    add Sentry to payment service

---
+++ src/paymentservice/sentry.js
@@ -0,0 +1,12 @@
+const Sentry = require("@sentry/node");
+
+export function initSentry() {
+  // Make sure to call `Sentry.init` BEFORE initializing the OpenTelemetry SDK
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    tracesSampleRate: 1.0,
+    // set the instrumenter to use OpenTelemetry instead of Sentry
+    instrumenter: "otel",
+    // ...
+  });
+}
